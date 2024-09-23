f09e0d24d6cab1a6b69b1fcbb706f671e93f22a3
zhonghuashu <77599567@qq.com>
2024-09-23 21:13:28 +0800

    Add timer test

--- timertest/timer_test.cpp
+++ timertest/timer_test.cpp
@@ -1,8 +1,8 @@
 #include <csignal>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <unistd.h>
-#include <time.h>
+#include <sys/time.h>
+#include <getopt.h>
 
 namespace {
 
@@ -33,20 +33,75 @@ void busyCpu()
 
 }
 
+void usleepTimeTest()
+{
+    int count = 0;
+    int i = 0;
+    ::printf("Start usleep timer test\n");
+    const struct timespec ts = {
+          .tv_sec = 0,
+          .tv_nsec = 500 * 1000     // 500 us
+    };
+
+    while(true)
+    {
+        // ::usleep(500);
+        ::nanosleep(&ts, nullptr);
+        busyCpu();
+        if (i++ == 2000)
+        {
+            ::printf("usleep count: %d\n", count);
+            i = 0;
+            count++;
+        }
+    }
+}
+
+void iTimerTestSignal(int signum)
+{
+    static int i = 0;
+    static int count = 0;
+
+    busyCpu();
+    if (i++ == 2000)
+    {
+        ::printf("itimer: %d\n", count++);
+        i = 0;
+    }
+}
+
+void iTimerTest()
+{
+    const struct timeval timeVal = {
+          .tv_sec = 0,
+          .tv_usec = 500 // 500 us
+    };
+
+    const itimerval its =
+    {
+        .it_interval = timeVal,
+        .it_value = timeVal
+    };
+
+    ::signal(SIGALRM, iTimerTestSignal);
+
+    if (::setitimer(ITIMER_REAL, &its, nullptr) == -1)
+    {
+        ::perror("setitimer failed");
+    }
+
+    while (true)
+    {
+        ::sleep(1);
+    }
+
+}
+
 void posixTimerSignal(int sig, siginfo_t *si, void *pri)
 {
     static int i = 0;
     static int count = 0;
 
-    /*
-    Test result: ./vmstat 1
-    procs -----------memory---------- ---swap-- -----io---- -system-- ------cpu-----
-    r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa st
-    1  0      0 439600   3408  37992    0    0     0     0  213  705  1  2 98  0  0
-    1  0      0 439408   3416  37996    0    0     0     0 2288 6947 38  1 61  0  0 >> Restart serval times, CPU load increased.
-    1  0      0 439408   3416  37996    0    0     0     0 2290 6798 38  0 62  0  0
-    1  0      0 439408   3416  37996    0    0     0     0 2301 6915 37  1 61  0  0
-    */
     busyCpu();
     if (i++ == 2000)
     {
@@ -106,10 +161,105 @@ void posixTimerTest()
 
 }
 
-}
-int main(int argc, char* argv[])
+void selectTimeTest()
 {
-    ::printf("Timer test cpp program started\n");
-    posixTimerTest();
-    return 0;
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 500;
+    static int i = 0;
+    static int count = 0;
+
+    while (true)
+    {
+        if (0 == ::select(0, nullptr, nullptr, nullptr, &tv))
+        {
+            busyCpu();
+            if (i++ == 2000)
+            {
+                ::printf("select_time: %d\n", count++);
+                i = 0;
+            }
+        }
+    }
+}
+
+void printUsage()
+{
+    ::printf("Usage: timer_test [options]\n");
+    ::printf("Options: \n");
+    ::printf(" -h, --help                    print help info\n");
+    ::printf(" -u, --usleep                  using usleep timer\n");
+    ::printf(" -i, --itimer                  using itimer\n");
+    ::printf(" -p, --ptimer                  using posix timer\n");
+    ::printf(" -s, --select                  using select timer\n");
+}
+
+bool parseArgs(const int argc, char *argv[])
+{
+    bool ret = true;
+    int opt;
+    const struct option longopts[] =
+    {
+        {"help",        0, nullptr, 'h'},
+        {"usleep",      0, nullptr, 'u'},
+        {"itimer",      0, nullptr, 'i'},
+        {"ptimer",      0, nullptr, 'p'},
+        {"select",      0, nullptr, 's'},
+        {nullptr,       0, nullptr, 0}
+    };
+
+    if (argc < 2)
+    {
+        ::printf("Program need an argument\n");
+        return false;
+    }
+
+    while ((opt = ::getopt_long(argc, argv, ":huips", longopts, nullptr)) != -1)
+    {
+        switch (opt)
+        {
+            case 'h':
+                printUsage();
+                break;
+            case 'u':
+                usleepTimeTest();
+                break;
+            case 'i':
+                iTimerTest();
+                break;
+            case 'p':
+                posixTimerTest();
+                break;
+            case 's':
+                selectTimeTest();
+                break;
+            case ':':
+                ::printf("Option needs a value\n");
+                ret = false;
+                break;
+            case '?':
+                ::printf("Unknown option: %c\n", ::optopt);
+                ret = false;
+                break;
+            default:
+                printUsage();
+                ret = false;
+                break;
+        }
+    }
+
+    return ret;
+}
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (!parseArgs(argc, argv))
+    {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
